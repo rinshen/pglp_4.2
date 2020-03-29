7cd67397772d62100a3a34f7fa61e12fed8860da
Francois Pic <francois.pic@ens.uvsq.fr>
2020-03-29 23:38:40 +0200

    Correction d'un bug dans la fonction divison

--- src/main/java/fr/uvsq/rinshen/ex42/MoteurRPN.java
+++ src/main/java/fr/uvsq/rinshen/ex42/MoteurRPN.java
@@ -2,40 +2,40 @@ package fr.uvsq.rinshen.ex42;
 
 public class MoteurRPN extends Interpreteur {
 
-	public MoteurRPN() {
+    public MoteurRPN() {
 		super();
 	}
-	
+
 	public void ajoute_operande(double op){
 		pile.add(op);
 	}
-	
+
 	public double pop() {
 		double res=pile.get(pile.size()-1);
 		pile.remove(pile.size()-1);
 		return res;
 	}
-	
+
 	public void addition(double op1, double op2) {
 		double res=op1+op2;
 		pile.add(res);
 	}
-	
+
 	public void soustraction(double op1, double op2) {
 		double res=op2-op1;
 		pile.add(res);
 	}
-	
+
 	public void multiplication(double op1, double op2) {
 		double res=op1*op2;
 		pile.add(res);
 	}
 
 	public void division(double op1, double op2) {
-		double res=op1/op2;
+		double res=op2/op1;
 		pile.add(res);
 	}
-	
+
 	public void execute_operateur(String op) {
 		double op1;
 		double op2;
@@ -70,9 +70,9 @@ public class MoteurRPN extends Interpreteur {
 				System.out.println("operation non reconnue");
 		}
 	}
-	
+
 	public void affiche_pile() {
-		for(int i=0;i<pile.size();i++) {
+		for (int i = 0; i < pile.size(); i++) {
 			System.out.print(pile.get(i) + " ");
 		}
 		System.out.print("\n");
--- src/test/java/fr/uvsq/rinshen/ex42/TestMoteurRPN.java
+++ src/test/java/fr/uvsq/rinshen/ex42/TestMoteurRPN.java
@@ -10,7 +10,7 @@ public class TestMoteurRPN {
 	public void test_ajoute_operande() {
 		MoteurRPN moteur=new MoteurRPN();
 		double val = 5;
-		double test = 0;
+		double test = val;
 		moteur.ajoute_operande(val);
 		double res = moteur.pop();
 		assertEquals(test, res, 0);
@@ -34,7 +34,7 @@ public class TestMoteurRPN {
 		MoteurRPN moteur=new MoteurRPN();
 		double val1 = 5;
 		double val2 = 4;
-		double test = -1;
+		double test = 1;
 		moteur.ajoute_operande(val1);
 		moteur.ajoute_operande(val2);
 		moteur.execute_operateur("-");
